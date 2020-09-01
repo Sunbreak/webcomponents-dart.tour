8adfef5da7d7131c31c2da3e3b7c4ac78a9f0027
Sunbreak <sunbreak.wang@gmail.com>
2020-09-01 16:34:11 +0800

    WIP: https://stackoverflow.com/questions/25295022/dart-custom-elements-vanilla-dart-without-polymer/63684036
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

--- web/main.dart
+++ web/main.dart
@@ -2,4 +2,18 @@ import 'dart:html';
 
 void main() {
   querySelector('#output').text = 'Your Dart app is running.';
+  print('main ${CoolHeading}');
+  window.customElements.define('cool-heading', () => CoolHeading());
+}
+
+class CoolHeading extends HtmlElement {
+  factory CoolHeading() => Element.tag('cool-heading');
+
+  CoolHeading.created() : super.created();
+
+  @override
+  void attached() {
+    super.attached();
+    print('cool heading attached');
+  }
 }
